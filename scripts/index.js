e24be70f459f6636bfafcc319590bf951860d27d
Merosque <norboevbair@gmail.com>
2025-03-23 20:31:20 +0900

    Создайте карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,25 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
+
+initialCards.forEach(function(element) {
+  // клонируем содержимое тега template
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  // наполняем содержимым
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__title').textContent = element.name;
+  // добавляем на страницу
+  placesList.append(cardElement);
+});
+
+
+
+
+
+
+
+
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
