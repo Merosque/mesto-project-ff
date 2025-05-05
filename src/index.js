e645e56a56e4ded10ae7efd61bf023ed88712365
Merosque <norboevbair@gmail.com>
2025-05-05 17:35:36 +0900

    4. Редактирование имени и информации о себе

--- src/index.js
+++ src/index.js
@@ -27,3 +27,35 @@ function createCard(element) {
 
 initialCards.forEach(renderCard);
 
+// Находим форму в DOM
+const formElement = document.querySelector(".popup__form[name='edit-profile']");
+// Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+const nameInput = document.querySelector('.popup__input_type_name')
+// Воспользуйтесь инструментом .querySelector()
+const jobInput = document.querySelector('.popup__input_type_description')
+// Воспользуйтесь инструментом .querySelector()
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    // Так мы можем определить свою логику отправки.
+    // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    const userJob = jobInput.value;
+    const userName = nameInput.value;
+       
+    // Выберите элементы, куда должны быть вставлены значения полей
+    const profileTitle = document.querySelector(".profile__title");
+    const profileDescription = document.querySelector(".profile__description");
+
+    // Вставьте новые значения с помощью textContent
+    profileDescription.textContent = userJob;
+    profileTitle.textContent = userName;
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
